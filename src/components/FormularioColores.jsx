eff5847a790a170337e30819199fa44bca7703d5
Nair Paez <nairpaez@outlook.com>
2026-04-18 11:12:35 -0300

    maquetado ListaColores

--- src/components/FormularioColores.jsx
+++ src/components/FormularioColores.jsx
@@ -1,19 +1,26 @@
+import ListaColores from "./ListaColores";
+
 const FormularioColores = () => {
   return (
-    <section className=" container border border-1 p-3 d-flex justify-content-center align-items-center gap-3">
-      <div className="caja-formulario"></div>
-      <form>
-        <div className="mb-3 d-flex justify-content-center gap-1 ">
-          <input
-            type="text"
-            className="form-control-lg"
-            id="inputColor"
-            placeholder="Ingresa un color ej.blue"
-          />
-          <button className="btn btn-success btn-lg">Agregar</button>
-        </div>
-      </form>
-    </section>
+    <>
+      <section className=" container  border border-1 p-3 d-flex flex-column flex-md-row justify-content-center align-items-center gap-3">
+        <div className="caja-formulario"></div>
+        <form>
+          <div className="mb-3 d-flex flex-column flex-md-row justify-content-center gap-1 ">
+            <input
+              type="text"
+              className="form-control-lg"
+              id="inputColor"
+              placeholder="Ingresa un color ej.blue"
+            />
+            <button className="btn btn-success btn-lg">Agregar</button>
+          </div>
+        </form>
+      </section>
+      <section>
+        <ListaColores></ListaColores>
+      </section>
+    </>
   );
 };
 
---
+++ src/components/ListaColores.jsx
@@ -0,0 +1,9 @@
+const ListaColores = () => {
+  return (
+    <section className="container my-2 border border-1">
+      <div className="row">lista colores</div>
+    </section>
+  );
+};
+
+export default ListaColores;
