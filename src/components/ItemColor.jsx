13a4695b85d18260f86b63993a913e52a25cbff9
Nair Paez <nairpaez@outlook.com>
2026-04-18 18:43:07 -0300

    maquetado componente ItemColor

--- src/components/ItemColor.jsx
+++ src/components/ItemColor.jsx
@@ -1,12 +1,12 @@
 const ItemColor = () => {
   return (
     <div className="col-6 col-md-4 col-lg-3">
-      <div class="card mt-3">
-        <div class="card-body border-0 d-flex flex-column align-items-center row-gap-2">
-          <h5 class="card-title">Nombre color</h5>
+      <div className="card mt-3">
+        <div className="card-body border-0 d-flex flex-column align-items-center row-gap-2">
+          <h5 className="card-title">Nombre color</h5>
           <div className="caja-colorItem"></div>
           <div>
-            <button class="btn btn-danger">Eliminar</button>
+            <button className="btn btn-danger">Eliminar</button>
           </div>
         </div>
       </div>
