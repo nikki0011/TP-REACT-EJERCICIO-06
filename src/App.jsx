ede7545704953e96d2edd76ee5cca3f9fd0ad651
Nair Paez <nairpaez@outlook.com>
2026-04-18 10:22:41 -0300

    maquetado FormularioColores

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import FormularioColores from "./components/FormularioColores";
+
 function App() {
   return (
     <>
       <main className="container my-3">
-        <h1>hola mundo</h1>
+        <h1 className='text-center my-4'>AdminColors</h1>
+        <FormularioColores></FormularioColores>
       </main>
     </>
   );
---
+++ src/components/FormularioColores.jsx
@@ -0,0 +1,20 @@
+const FormularioColores = () => {
+  return (
+    <section className=" container border border-1 p-3 d-flex justify-content-center align-items-center gap-3">
+      <div className="caja-formulario"></div>
+      <form>
+        <div className="mb-3 d-flex justify-content-center gap-1 ">
+          <input
+            type="text"
+            className="form-control-lg"
+            id="inputColor"
+            placeholder="Ingresa un color ej.blue"
+          />
+          <button className="btn btn-success btn-lg">Agregar</button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default FormularioColores;
