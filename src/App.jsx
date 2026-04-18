ff48e2870c9e2e21f3204371bc518c7c49222f64
Nair Paez <nairpaez@outlook.com>
2026-04-18 10:30:36 -0300

    maquetado NavBar- Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import Footer from "./components/Footer";
 import FormularioColores from "./components/FormularioColores";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
+    <Navbar></Navbar>
       <main className="container my-3">
         <h1 className='text-center my-4'>AdminColors</h1>
         <FormularioColores></FormularioColores>
       </main>
+      <Footer></Footer>
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="bg-dark-subtle py-4 text-center">
+            <p>&copy; Todos los derechos reservados</p>
+        </footer>
+    );
+};
+
+export default Footer;
